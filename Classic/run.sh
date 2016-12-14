66abed4d3af945d24ac957a84fd32b598ba6405e
neighborzhang <tianyi.zhang1991@gmail.com>
2016-12-14 04:05:08 -0600

    Add files via upload

---
+++ Classic/run.sh
@@ -0,0 +1,4 @@
+python main.py CartPole-v0 > cartpole_train_result
+python main.py Pendulum-v0 > pendulum_train_result
+python main.py Acrobot-v1 > acrobot_train_result
+python main.py MountainCar-v0 > mountain_train_result
---
+++ Classic/run_test.sh
@@ -0,0 +1,4 @@
+python main_test.py CartPole-v0 > cartpole_test_result
+python main_test.py Pendulum-v0 > pendulum_test_result
+python main_test.py Acrobot-v1 > acrobot_test_result
+python main_test.py MountainCar-v0 > mountaincar_test_result
